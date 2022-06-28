c737fcd27e3859c9b2f836fcbb9b0243d1b170ce
Dennis Frankenbach <dennisfrankenbach@gmail.com>
2022-06-28 23:26:47 +0200

    feat: update og images and text

--- components/seo.tsx
+++ components/seo.tsx
@@ -1,24 +1,19 @@
 import { DefaultSeo } from 'next-seo'
 
 const config = {
-  title: 'eBike Routes: Curated running & hiking routes',
-  description: 'Explore curated trails in Sweden for hiking & running.',
+  title: 'eBike Routes of Dennis Frankenbach',
+  description: 'Explore my routes.',
   openGraph: {
     type: 'website',
     locale: 'en_US',
-    url: 'https://routes.samuelkraft.com',
+    url: 'https://ebike.dennisfrankenbach.me/',
     site_name: 'eBike Routes',
     images: [
       {
-        url: 'https://routes.samuelkraft.com/og.png',
-        alt: 'Trail Routes',
+        url: 'https://ebike.dennisfrankenbach.me/og.png',
+        alt: 'eBike Routes',
       },
     ],
-  },
-  twitter: {
-    handle: '@samuelkraft',
-    site: '@samuelkraft',
-    cardType: 'summary_large_image',
   },
 }
 
